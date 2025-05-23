a279aaf56119a4f53dad3697585a84235ea3b68e
Kaya Luken <kayaluken@gmail.com>
2025-05-23 17:21:18 +0200

    exit on exit cmd

--- app/main.go
+++ app/main.go
@@ -19,6 +19,11 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		// Remove trailing newline
+		command = command[:len(command)-1]
+		if command == "exit" {
+			os.Exit(0)
+		}
+		fmt.Println(command + ": command not found")
 	}
 }
